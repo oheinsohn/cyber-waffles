5b2180337ff4da2f4de96fa63114ff5132dcd082
Oliver Heinsohn <oliver.heinsohn@gmail.com>
2022-07-04 22:59:17 +0200

    C Added file read

--- main.c
+++ main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 
+void readScopeFromJSON()
+{
+    FILE *scopeFile;
+    scopeFile = fopen("scope.json","r");
+
+    char scopeFileBuffer[10000];
+    fread(scopeFileBuffer, 10000, 1, scopeFile);
+	fclose(scopeFile);
+
+    // No general parsing of this JSON - just for this case
+    printf("%s\n", scopeFileBuffer);
+}
+
 void displayScope()
 {
     char host[10000] = "localhost";
     char IP[10000] = "127.0.0.1";
+
+    readScopeFromJSON();
 
     printf("Host: %s\n", host);
     printf("IP: %s\n", IP);
