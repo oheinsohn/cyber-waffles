ee2519b606da018618fa36d6e12d790fbf3b0157
Oliver Heinsohn <oliver.heinsohn@gmail.com>
2022-07-11 22:27:36 +0200

    Added splitting for JSON parser

--- c/JSONParser.h
+++ c/JSONParser.h
@@ -1,6 +1,25 @@
+#include <string.h>
 
-
+// No general parsing of this JSON - just for this context    
 void parseJSON(char *scopeFileBuffer){
-    // No general parsing of this JSON - just for this case
-    printf("%s\n", scopeFileBuffer);
+    
+    // show all of the JSON
+    // printf("%s\n", scopeFileBuffer);
+   
+    char *scopeEntry;
+    char *arrayEntries[3];
+    // first entry is just "scope" - no real entry
+    scopeEntry = strtok(scopeFileBuffer, "{");
+    scopeEntry = strtok(NULL, "{");
+    int entryCounter = 0;
+    while(scopeEntry != NULL) {
+        arrayEntries[entryCounter++] = scopeEntry;    
+        scopeEntry = strtok(NULL, "{");
+    }
+    for(int displayArrayCounter = 0; 
+        displayArrayCounter < entryCounter; 
+        ++displayArrayCounter
+        ) {
+            printf("%s\n", arrayEntries[displayArrayCounter]);
+    }
 }
